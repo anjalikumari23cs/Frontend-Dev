18fa4fb7c5a876f0c2c1bbe613558bb9df209d59
anjalikumari23cs <anjalikumari93680@gmail.com>
2025-11-12 10:49:26 +0530

    Question3 Added

---
+++ Assignment2/Math_Utility_DashBoard.js
@@ -0,0 +1,5 @@
+let num=16.75;
+console.log("Rounded Value:",Math.round(num).toFixed(2));
+console.log("SquareRoot Value:",Math.sqrt(num).toFixed(2));
+console.log("Power of 3 Value:",Math.pow(num,3).toFixed(2));
+console.log("Randome Value:",(Math.random()*41+10).toFixed(2));
---
+++ Assignment2/String_Manipulation_report.js
@@ -0,0 +1,8 @@
+let str=" wireless headphones PRO";
+let s1=str.trim();
+console.log("Original String:" + str);
+console.log("Length of the OG string: " + str.length);
+console.log("After removing leading and trailing spaces:" + s1);
+console.log("Length after removing leading and trailing spaces:",s1.length);
+console.log("Converting in Lower Case: ",s1.toLocaleLowerCase());
+console.log("Capatilize first letter: ",s1);
